aee3b9eec162ed192d06069e2899dbaa8e7c7201
Nili Scharf <nilischarf@gmail.com>
2025-02-25 10:10:38 +0200

    complete fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    text_file = f"{file_name}.txt"
+    
+    with open(text_file, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    text_file = f"{file_name}.txt"
+
+    with open(text_file, mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    text_file = f"{file_name}.txt"
+
+    with open(text_file, encoding='utf-8') as file:
+        return file.read()
+
